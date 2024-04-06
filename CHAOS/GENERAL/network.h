6add50b4f95a225fcfb0906b9488f3681e052381
ef71a0518ebc075a7f309cca8 <163979047+ef71a0518ebc075a7f309cca8@users.noreply.github.com>
2024-04-06 05:55:57 -0500

    Create network.h

---
+++ CHAOS/GENERAL/network.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <thread>
+#include <array>
+#include <map>
+#include <string>
+#include <cmath>
+
+#include "init.h"
+#include "cryptic.h"
+
+int establish() {
+	return 0;
+}
