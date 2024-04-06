fcae825e3ea883c0478d2c65096062eab983466e
ef71a0518ebc075a7f309cca8 <163979047+ef71a0518ebc075a7f309cca8@users.noreply.github.com>
2024-04-06 05:56:15 -0500

    Create cryptic.h

---
+++ CHAOS/GENERAL/cryptic.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <thread>
+#include <array>
+#include <map>
+#include <string>
+#include <cmath>
+
+#include "network.h"
+#include "init.h"
+
+int crypticinit() {
+	return 28346;
+}
